16d2faf87233b02cb60d88ae0ecf4b75eeae76a9
academiaendigital <academiaendigital@gmail.com>
2026-05-10 14:06:58 -0400

    clase 1

---
+++ src/js/01-holamundo.js
@@ -0,0 +1,28 @@
+/*alert("hola mundo");*/
+console.log("Estoy aprendiendo JAVA SCRIPT");
+var nombre = 'Jenny';
+var edad = 42;
+console.log("Hola " + nombre + " y tienes " + edad + " años");
+
+let num1= 15;
+let num2= 25;
+let suma = num1 +num2;
+//forma antigua de imprimir en consola 
+console.log("La suma de " +num1 + " mas " + num2 + " es igual a " + suma);
+// template literales
+console.log(`La suma de ${num1} mas ${num2} es igual a ${suma}`);
+console.log(`La suma de ${num1} mas ${num2} es igual a ${num1+num2}`);
+console.log(`La Resta  de ${num1} menos ${num2} es igual a ${num1-num2}`);
+console.log(`La Multiplicacion de ${num1} por ${num2} es igual a ${num1*num2}`);
+console.log(`La Division de ${num1} entre ${num2} es igual a ${num1/num2}`);
+
+let ciudad="caracas"; //inicializar una variable
+console.log(ciudad);
+ciudad ="san juan";
+console.log(ciudad);
+
+var ciudad2="maracay";
+console.log(ciudad2);
+// variables declaradas var y let si le puedo reasignar un nuevo valor
+const pi= 3.14; // no puedo reasignarle su valor 
+console.log(pi);
